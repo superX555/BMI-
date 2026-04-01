7e38294ed78d046eadec64c05bf58fd4f26c965f
LUKE <LUKE@LAPTOP-FJV5DOFD>
2026-04-01 15:11:34 +0800

    加入專案檔案。

---
+++ BMI計算機/frmBMI.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace BMI計算機
+{
+    partial class frmBMI
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        private void InitializeComponent()
+        {
+            this.groupInput = new System.Windows.Forms.GroupBox();
+            this.btnOpenLog = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.grpOutput = new System.Windows.Forms.GroupBox();
+            this.dgvHistory = new System.Windows.Forms.DataGridView();
+            this.lblBMITitle = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.groupInput.SuspendLayout();
+            this.grpOutput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupInput
+            // 
+            this.groupInput.Controls.Add(this.btnOpenLog);
+            this.groupInput.Controls.Add(this.lblName);
+            this.groupInput.Controls.Add(this.txtName);
+            this.groupInput.Controls.Add(this.lblHeight);
+            this.groupInput.Controls.Add(this.txtHeight);
+            this.groupInput.Controls.Add(this.lblWeight);
+            this.groupInput.Controls.Add(this.txtWeight);
+            this.groupInput.Controls.Add(this.btnCalc);
+            this.groupInput.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupInput.Location = new System.Drawing.Point(12, 12);
+            this.groupInput.Name = "groupInput";
+            this.groupInput.Size = new System.Drawing.Size(530, 180);
+            this.groupInput.TabIndex = 0;
+            this.groupInput.TabStop = false;
+            this.groupInput.Text = " 個人資料輸入 ";
+            // 
+            // btnOpenLog
+            // 
+            this.btnOpenLog.BackColor = System.Drawing.Color.White;
+            this.btnOpenLog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpenLog.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnOpenLog.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnOpenLog.Location = new System.Drawing.Point(375, 122);
+            this.btnOpenLog.Name = "btnOpenLog";
+            this.btnOpenLog.Size = new System.Drawing.Size(135, 32);
+            this.btnOpenLog.TabIndex = 5;
+            this.btnOpenLog.Text = "查看歷史紀錄";
+            this.btnOpenLog.UseVisualStyleBackColor = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblName.Location = new System.Drawing.Point(20, 35);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(39, 19);
+            this.lblName.Text = "姓名";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(110, 32);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(240, 27);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblHeight.Location = new System.Drawing.Point(20, 80);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(71, 19);
+            this.lblHeight.Text = "身高(cm)";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(110, 77);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(240, 27);
+            this.txtHeight.TabIndex = 2;
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblWeight.Location = new System.Drawing.Point(20, 125);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(67, 19);
+            this.lblWeight.Text = "體重(kg)";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(110, 122);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(240, 27);
+            this.txtWeight.TabIndex = 3;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnCalc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalc.ForeColor = System.Drawing.Color.White;
+            this.btnCalc.Location = new System.Drawing.Point(375, 32);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(135, 84);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "計算並儲存";
+            this.btnCalc.UseVisualStyleBackColor = false;
+            // 
+            // grpOutput
+            // 
+            this.grpOutput.Controls.Add(this.dgvHistory);
+            this.grpOutput.Controls.Add(this.lblBMITitle);
+            this.grpOutput.Controls.Add(this.lblResult);
+            this.grpOutput.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpOutput.Location = new System.Drawing.Point(12, 205);
+            this.grpOutput.Name = "grpOutput";
+            this.grpOutput.Size = new System.Drawing.Size(530, 300);
+            this.grpOutput.TabIndex = 1;
+            this.grpOutput.TabStop = false;
+            this.grpOutput.Text = " 計算結果與歷史預覽 ";
+            // 
+            // dgvHistory
+            // 
+            this.dgvHistory.AllowUserToAddRows = false;
+            this.dgvHistory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHistory.BackgroundColor = System.Drawing.Color.White;
+            this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistory.Location = new System.Drawing.Point(15, 105);
+            this.dgvHistory.Name = "dgvHistory";
+            this.dgvHistory.ReadOnly = true;
+            this.dgvHistory.RowHeadersVisible = false;
+            this.dgvHistory.Size = new System.Drawing.Size(500, 180);
+            this.dgvHistory.TabIndex = 1;
+            // 
+            // lblBMITitle
+            // 
+            this.lblBMITitle.AutoSize = true;
+            this.lblBMITitle.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblBMITitle.Location = new System.Drawing.Point(20, 50);
+            this.lblBMITitle.Name = "lblBMITitle";
+            this.lblBMITitle.Size = new System.Drawing.Size(134, 19);
+            this.lblBMITitle.Text = "身體質量指數(BMI)";
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.Color.White;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblResult.ForeColor = System.Drawing.Color.Gray;
+            this.lblResult.Location = new System.Drawing.Point(210, 35);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(300, 60);
+            this.lblResult.Text = "0.00 (待計算)";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmBMI
+            // 
+            this.AcceptButton = this.btnCalc;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(555, 520);
+            this.Controls.Add(this.grpOutput);
+            this.Controls.Add(this.groupInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmBMI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMI 健康追蹤系統 v2.0";
+            this.groupInput.ResumeLayout(false);
+            this.groupInput.PerformLayout();
+            this.grpOutput.ResumeLayout(false);
+            this.grpOutput.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupInput;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnOpenLog;
+        private System.Windows.Forms.GroupBox grpOutput;
+        private System.Windows.Forms.Label lblBMITitle;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.DataGridView dgvHistory;
+    }
+}
+
---
+++ BMI計算機/frmBMI.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BMI計算機
+{
+    public partial class frmBMI : Form
+    {
+        private string logFile = "BMI_Records.txt";
+
+        public frmBMI()
+        {
+            InitializeComponent();
+
+            // 初始化表格欄位
+            InitHistoryGrid();
+
+            // 手動綁定按鈕事件
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            this.btnOpenLog.Click += new System.EventHandler(this.btnOpenLog_Click);
+        }
+
+        private void InitHistoryGrid()
+        {
+            if (dgvHistory.Columns.Count == 0)
+            {
+                dgvHistory.Columns.Add("Time", "時間");
+                dgvHistory.Columns.Add("Name", "姓名");
+                dgvHistory.Columns.Add("Height", "身高"); // 新增身高欄位
+                dgvHistory.Columns.Add("Weight", "體重"); // 新增體重欄位
+                dgvHistory.Columns.Add("BMI", "BMI");
+                dgvHistory.Columns.Add("Status", "診斷");
+
+                dgvHistory.DefaultCellStyle.Font = new Font("微軟正黑體", 10);
+                dgvHistory.ColumnHeadersDefaultCellStyle.Font = new Font("微軟正黑體", 10, FontStyle.Bold);
+
+                // 自動調整欄位權重，讓顯示更平均
+                dgvHistory.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txtName.Text.Trim();
+                if (string.IsNullOrEmpty(name))
+                {
+                    MessageBox.Show("請輸入姓名！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                double h_cm = Convert.ToDouble(txtHeight.Text);
+                double weight = Convert.ToDouble(txtWeight.Text);
+                double h_m = h_cm / 100;
+                double bmi = weight / (h_m * h_m);
+
+                string status = "";
+                Color displayColor = Color.Black;
+                bool isAbnormal = false;
+
+                // 核心判定邏輯
+                if (bmi < 18.5) { status = "體重過輕"; displayColor = Color.Blue; isAbnormal = true; }
+                else if (bmi < 24) { status = "正常範圍"; displayColor = Color.Green; isAbnormal = false; }
+                else if (bmi < 27) { status = "過重"; displayColor = Color.Orange; isAbnormal = true; }
+                else { status = "肥胖"; displayColor = Color.Red; isAbnormal = true; }
+
+                // 更新大框框結果
+                lblResult.Text = $"{bmi:F2} ({status})";
+                lblResult.ForeColor = displayColor;
+
+                // 執行存檔與介面更新 (傳入身高與體重參數)
+                SaveToLog(name, h_cm, weight, bmi, status);
+                AddRecordToGrid(name, h_cm, weight, bmi, status, isAbnormal);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("請輸入數值！", "格式錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void AddRecordToGrid(string name, double h, double w, double bmi, string status, bool highlight)
+        {
+            // 在表格中新增包含身高與體重的行
+            int n = dgvHistory.Rows.Add(
+                DateTime.Now.ToString("HH:mm:ss"),
+                name,
+                h.ToString() + " cm",
+                w.ToString() + " kg",
+                bmi.ToString("F2"),
+                status
+            );
+
+            if (highlight)
+            {
+                dgvHistory.Rows[n].DefaultCellStyle.ForeColor = Color.Red;
+                dgvHistory.Rows[n].DefaultCellStyle.Font = new Font("微軟正黑體", 10, FontStyle.Bold);
+                dgvHistory.Rows[n].DefaultCellStyle.SelectionForeColor = Color.Red;
+            }
+            dgvHistory.FirstDisplayedScrollingRowIndex = n;
+        }
+
+        private void btnOpenLog_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(logFile))
+                Process.Start(new ProcessStartInfo { FileName = "notepad.exe", Arguments = Path.GetFullPath(logFile), UseShellExecute = true });
+            else
+                MessageBox.Show("尚無紀錄。");
+        }
+
+        private void SaveToLog(string name, double h, double w, double bmi, string status)
+        {
+            try
+            {
+                // 在 TXT 檔案中也加入身高與體重紀錄，並調整對齊
+                string record = $"{DateTime.Now:yyyy/MM/dd HH:mm:ss} | 姓名:{name,-8} | 身高:{h,5}cm | 體重:{w,5}kg | BMI:{bmi,6:F2} | {status}";
+                File.AppendAllLines(logFile, new string[] { record });
+            }
+            catch { }
+        }
+
+        private void frmBMI_Load(object sender, EventArgs e) { }
+    }
+}
